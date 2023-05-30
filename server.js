b9ff61f35d82b6068b1c7bacd9a19c4000635c18
Ravi Lour <ravijaat2917@gmail.com>
2023-05-30 15:20:18 +0530

    production code added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import connectDB from "./DataBase/connectDB.js";
 import morgan from "morgan";
 import userRoutes from "./Routes/user.Routes.js";
 import listRoutes from "./Routes/lists.Routes.js";
+import path from 'path';
+import { fileURLToPath } from "url";
 
 // Creating App Instance from Express
 const app = express();
@@ -15,12 +17,21 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(path.join(__dirname));
+app.use(express.static(path.join(__dirname,"./client/build")));
+
 // Connecting Database (MongoDB)
 connectDB();
 
 // Making Routes
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", listRoutes);
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+});
 
 // Creating Port for Listening
 const PORT = process.env.PORT;
